2ca2e0446ea43114dd46f4ea2232179f5da4536b
fciuculescu <fciuculescu@deloittece.com>
2020-09-05 17:02:06 +0300

    Updated profiles for JPA services. closes #48

--- pet-clinic-data/src/main/java/com/example/petclinic/services/map/SpecialityMapService.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/SpecialityMapService.java
@@ -2,11 +2,13 @@ package com.example.petclinic.services.map;
 
 import com.example.petclinic.model.Speciality;
 import com.example.petclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialtyService {
     @Override
     public Set<Speciality> findAll() {
